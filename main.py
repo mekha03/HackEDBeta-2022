f915c09a1a7071776c3e61a2b28e1d05dba87c23
mekha03 <mekha@ualberta.ca>
2022-11-05 17:52:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,10 +27,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                #  if player_rect.bottom == 450:
-                    player_gravity = -20  # Remove for mario game
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    #  if player_rect.bottom == 450:
+                        player_gravity = -20  # Remove for mario game
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                homework_rect.left = 800
+                text = test_font.render('Super Engg Girlies', True, 'Black')
+                text_rect = text.get_rect(center=(400, 100))
+
     # draw stuff :p
     if game_active:
         screen.blit(background, (0, 0))
@@ -50,11 +58,17 @@ while True:
 
         if homework_rect.colliderect(player_rect):
             game_active = False
-            screen.fill('grey1')
-            text = test_font.render('Game Over', True, 'White')
-            text_rect = text.get_rect(center=(400, 225))
-            screen.blit(text, text_rect)
-
+            if game_active == False:
+                screen.fill('grey1')
+                text = test_font.render('Game Over', True, 'White')
+                text2 = test_font.render('Press Space-bar to', True, 'White')
+                text3 = test_font.render('RESTART', True, 'White')
+                text_rect = text.get_rect(center=(400, 200))
+                text2_rect = text.get_rect(center=(325, 275))
+                text3_rect = text.get_rect(center=(400, 350))
+                screen.blit(text, text_rect)
+                screen.blit(text2, text2_rect)
+                screen.blit(text3, text3_rect)
     # if player_rect.colliderect(homework_rect):
     pygame.display.update()
     clock.tick(60)
