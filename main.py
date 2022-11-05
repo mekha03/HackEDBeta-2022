d6cf37db5d3f7045113b47fba5abe24496cf3131
mekha03 <mekha@ualberta.ca>
2022-11-05 15:56:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,22 @@ ground.fill('snow')
 text = test_font.render('Super Engg Girlies', True, 'Black')
 text_rect = text.get_rect(center = (400,100))
 
-homework = pygame.image.load('graphics/homework.png').convert_alpha()
-homework_rect = homework.get_rect(topleft=(800, 400))
+homework = pygame.image.load('graphics/homework-.png').convert_alpha()
+homework_rect = homework.get_rect(bottomleft=(800, 500))
 
 player = pygame.image.load('graphics/Character_right.png').convert_alpha()
 player_rect = player.get_rect(bottomleft=(200, 450))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                #  if player_rect.bottom == 450:
+                    player_gravity = -20  # Remove for mario game
     # draw stuff :p
     screen.blit(background, (0, 0))
     screen.blit(ground, (0, 450))
@@ -34,7 +39,16 @@ while True:
     else:
         homework_rect.left -= 4
     screen.blit(homework, homework_rect)
+
+    player_gravity +=1
+    player_rect.bottom += player_gravity
+    if player_rect.bottom >=450:
+        player_rect.bottom = 450
     screen.blit(player, player_rect)
+
+    if homework_rect.colliderect(player_rect):
+        pygame.quit()
+        exit()
 
     # if player_rect.colliderect(homework_rect):
     pygame.display.update()
