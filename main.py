093e7b4e11917a778b9e65196488e8570a04b907
mekha03 <mekha@ualberta.ca>
2022-11-05 18:27:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('Super Engg Girlies')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/font.otf', 50)
+text_font = pygame.font.Font(None, 50)
 game_active = True
 
 background = pygame.image.load('graphics/16431.jpg').convert()
@@ -21,6 +22,10 @@ homework_rect = homework.get_rect(bottomleft=(800, 500))
 player = pygame.image.load('graphics/Character_right.png').convert_alpha()
 player_rect = player.get_rect(bottomleft=(200, 450))
 player_gravity = 0
+
+score = 0
+score_text = text_font.render(str(score), True, 'Black')
+score_rect = score_text.get_rect(center=(400, 175))
 
 while True:
     for event in pygame.event.get():
@@ -45,10 +50,12 @@ while True:
         screen.blit(ground, (0, 450))
         screen.blit(text, text_rect)
         if homework_rect.left < -100:
+            score = score + 1
             homework_rect.left = 800
         else:
             homework_rect.left -= 4
         screen.blit(homework, homework_rect)
+        screen.blit(score_text, score_rect)
 
         player_gravity +=1
         player_rect.bottom += player_gravity
@@ -60,7 +67,6 @@ while True:
             game_active = False
             if game_active == False:
                 screen.fill('grey1')
-                text_font = pygame.font.Font(None, 50)
                 text = text_font.render('Game Over', True, 'White')
                 text2 = text_font.render('Press Space-bar to', True, 'White')
                 text3 = text_font.render('RESTART', True, 'White')
